77f89964db08780244ad39a82805c2c4c2439145
BigCode07 <bigcode07@gmail.com>
2025-01-16 23:56:33 -0300

    Se muestra componente si el presupuesto es valido

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useMemo } from "react";
 import BudgeForm from "./components/BudgeForm";
+import { useBudget } from "./hooks/useBudget";
+import BudgetTracker from "./components/BudgetTracker";
 
 function App() {
+  const { state } = useBudget();
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
   return (
     <>
       <header className=" bg-blue-600 py-8 max-h-72">
@@ -10,7 +16,7 @@ function App() {
         </h1>
       </header>
       <div className=" max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgeForm />
+        {isValidBudget ? <BudgetTracker /> : <BudgeForm />}
       </div>
     </>
   );
